eadabc44513c802f0003f986748faaf58d8c5cf5
unknown <byungt02@naver.com>
2022-04-06 15:50:49 +0900

    [GT] 6

---
+++ Lecture05/viewHanoi.js
@@ -0,0 +1,6 @@
+var hanoi = document.getElementById("hm0");
+
+function init()
+{
+    hanoi.innerHTML = "<br><span class='gle'>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</span><br><span class='gle'>🟨🟨🟨🟨🟨🟨🟨🟨🟨</span><br><span class='gle'>🟩🟩🟩🟩🟩🟩🟩</span><br><span class='gle'>🟦🟦🟦🟦🟦</span><br><span class='gle'>🟪🟪🟪</span><br><span class='gle'>⬛</span>";
+}
