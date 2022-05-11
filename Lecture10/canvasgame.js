8766cd66699e8e7674f3835611c80915522ec589
unknown <byungt@naver.com>
2022-05-11 15:43:05 +0900

    [GT] 슈팅게임 재료는 다 갖춰졌다.

--- Lecture10/canvasgame.js
+++ Lecture10/canvasgame.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 
-function objDraw(x, y, size, color)
+function ObjDraw(x, y, size, color)
 {
     context.beginPath();
     context.arc(x - size, y - size, size, 0, 2 * Math.PI);
@@ -12,43 +12,65 @@ function objDraw(x, y, size, color)
 
 class player
 {
-    constructor(x, y, size, color)
+    constructor(x, y, size)
     {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.color = color;
     }
-    draw()
+    Draw()
     {
-        objDraw(this.x, this.y, this.size, this.color);
+        ObjDraw(this.x, this.y, this.size, "blue");
     }
 }
 
 class bullet
 {
-    constructor(x, y, size, color)
+    constructor(x, y, size)
     {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.color = color;
     }
-    draw()
+    Draw()
     {
-        objDraw(this.x, this.y, this.size, this.color);
+        ObjDraw(this.x, this.y, this.size, "red");
+    }
+    Move()
+    {
+        this.y--;
     }
 }
 
+class GameManager
+{
+    static Update()
+    {
+        for (let index = 0; index < b.length; index++)
+            b[index].Move();
+    }
 
-var p = new player(50, 50, 10, "blue");
-var b = [];
+    static Draw()
+    {
+        for (let index = 0; index < b.length; index++)
+            b[index].Draw();
+        p.Draw();
+    }
 
-p.draw();
+    Loop()
+    {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        GameManager.Update();
+        GameManager.Draw();
+    }
+}
+p = new player(320, 240, 15);
+b = [];
+e = [];
 canvas.onclick = function(event){
     const x = event.clientX;
     const y = event.clientY;
-    b.push(new bullet(x, y, 5, "red"));
-    p.draw();
-    b[b.length - 1].draw();
+    b.push(new bullet(x, y, 8));
 }
+var gm = new GameManager();
+var loop = setInterval(gm.Loop , 1000 / 60);
