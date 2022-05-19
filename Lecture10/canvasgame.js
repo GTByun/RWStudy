ce27d055e96613fa1793e077c6d9153b028dbd8a
GTByun <byungt02@naver.com>
2022-05-20 02:11:47 +0900

    [GT] 콜라이더 구현은 다음으로!

--- Lecture10/canvasgame.js
+++ Lecture10/canvasgame.js
@@ -1,193 +1,304 @@
-var canvas = document.getElementById("game");
-var context = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
-var halfWidth = canvas.width / 2;
-var halfHeight = canvas.height / 2;
-var VectorInitial = {x: null, y: null};
-var PI = Math.PI;
-
 class Vector
 {
-    constructor(vector)
+    static null = new Vector(null, null);
+    static zero = new Vector(0, 0);
+
+    constructor(x, y)
     {
-        this.vector = vector;
+        this.x = x;
+        this.y = y;
     }
 
-    CalVector(what, vector)
+    CalVector(operator, vector)
     {
-        switch (what)
+        switch (operator)
         {
+            case "+":
+                return new Vector(this.x + vector.x, this.y + vector.y);
             case "-":
-                return new Vector({x: this.vector.x - vector.vector.x, y: this.vector.y - vector.vector.y});
+                return new Vector(this.x - vector.x, this.y - vector.y);
             default:
-                return new Vector(VectorInitial);
+                return null;
         }
     }
     
-    CalVectorKeep(what, vector)
+    CalVectorKeep(operator, vector)
     {
-        switch (what)
+        switch (operator)
         {
             case "+":
-                this.vector = {x: this.vector.x + vector.vector.x, y: this.vector.y + vector.vector.y};
+                this.x = this.x + vector.x;
+                this.y = this.y + vector.y;
                 break;
             default:
                 break;
         }
     }
 
-    CalScalar(what, scalar)
+    CalScalar(operator, scalar)
     {
-        switch (what)
+        switch (operator)
         {
             case "*":
-                return new Vector({x: this.vector.x * scalar, y: this.vector.y * scalar});
+                return new Vector(this.x * scalar, this.y * scalar);
             case "/":
-                return new Vector({x: this.vector.x / scalar, y: this.vector.y / scalar});
+                return new Vector(this.x / scalar, this.y / scalar);
             default:
-                return new Vector(VectorInitial);
+                return null;
         }
+    }
+
+    DistanceFromZero()
+    {
+        var pow = Math.pow;
+        return Math.sqrt(pow(this.x, 2) + pow(this.y, 2));
     }
 
     Normalization()
     {
-        return this.CalScalar("/", Math.sqrt(Math.pow(this.vector.x, 2) + Math.pow(this.vector.y, 2)))
+        return this.CalScalar("/", this.DistanceFromZero())
     }
 
     Copy()
     {
-        return new Vector(this.vector);
+        return new Vector(this.x, this.y);
     }
 }
 
-function Hex2(int)
+class Canvas
 {
-    var result = int.toString(16);
-    return result.length == 1 ? "0" + result : result;
+    static self = document.getElementById("game");
+    static context = this.self.getContext("2d");
+    static width = this.self.width;
+    static height = this.self.height;
+    static width_Half = this.width / 2;
+    static height_Half = this.height / 2;
+    static collider = {type: "Box", start: new Vector(-this.width_Half, -this.height_Half), end: new Vector(this.width_Half, this.height_Half)};
+    static position = new Vector(this.width_Half, this.height_Half);
 }
 
-function RandomInt256()
+class Cal
 {
-    return Math.random() * 256 | 0;
+    static PI = Math.PI;
+
+    static Dec2Hex(dec, digit)
+    {
+        var result = dec.toString(16);
+        for (let index = result.length; index < digit; index++)
+            result = "0" + result;
+        return result;
+    }
 }
 
-function RGB(red, green, blue)
+class Draw
 {
-    return "#" + Hex2(red) + Hex2(green) + Hex2(blue);
+    static RGB(red, green, blue)
+    {
+        var Hex2 = function(dec) { return Cal.Dec2Hex(dec, 2); };
+        return "#" + Hex2(red) + Hex2(green) + Hex2(blue);
+    }
+
+    static Circle(position, size, color)
+    {
+        var context = Canvas.context;
+        context.beginPath();
+        context.arc(position.x, position.y, size, 0, 2 * Cal.PI);
+        context.fillStyle = color;
+        context.fill();
+        context.closePath();
+    }
 }
 
-function ObjDraw(position, size, color)
+class Random
 {
-    context.beginPath();
-    context.arc(position.vector.x, position.vector.y, size, 0, 2 * PI);
-    context.fillStyle = color;
-    context.fill();
-    context.closePath();
+    static RandomInt(limit)
+    {
+        return Math.random() * limit | 0;
+    }
+
+    static Random(limit)
+    {
+        return Math.random() * limit;
+    }
+
+    static RandomBool()
+    {
+        return Random.RandomInt(3) == 1 ? true : false;
+    }
 }
 
-class player
+class Player
 {
     constructor(position, size)
     {
         this.position = position;
         this.size = size;
     }
+
     Draw()
     {
-        ObjDraw(this.position, this.size, "blue");
+        Draw.Circle(this.position, this.size, "blue");
     }
 }
 
-class bullet
+class Bullet
 {
-    constructor(position, size, moveVector, speed)
+    constructor(position, size, direction, speed)
     {
         this.position = position;
         this.size = size;
-        this.halfSize = size / 2;
-        this.moveVector = moveVector.CalScalar("*", speed);
+        this.velocity = direction.CalScalar("*", speed);
+        this.collider = {type: "Circle", size: this.size, offset: Vector.zero};
     }
+
     Draw()
     {
-        ObjDraw(this.position, this.size, "red");
+        Draw.Circle(this.position, this.size, "red");
     }
+
     Move()
     {
-        this.position.CalVectorKeep("+", this.moveVector);
+        this.position.CalVectorKeep("+", this.velocity);
     }
 }
 
 class Enemy
 {
+    static sizeTimes = 3;
+
     constructor(position, hp, speed, player)
     {
         this.position = position;
         this.hp = hp;
-        this.moveVector = player.position.CalVector("-", this.position).Normalization().CalScalar("*", speed)
-        this.color = RGB(RandomInt256(), RandomInt256(), RandomInt256());
+        this.sizeTimes = Enemy.sizeTimes;
+        this.size = hp * this.sizeTimes;
+        this.velocity = player.position.CalVector("-", this.position).Normalization().CalScalar("*", speed)
+        var RandomColor = function() { return Random.RandomInt(230) };
+        this.color = Draw.RGB(RandomColor(), RandomColor(), RandomColor());
+        this.collider = {type: "Circle", size: this.size, offset: Vector.zero};
     }
+
+    UpdateSize()
+    {
+        this.size = this.hp * this.sizeTimes;
+        this.collider.size = this.size;
+    }
+
     Draw()
     {
-        this.size = this.hp * 3;
-        ObjDraw(this.position, this.size, this.color);
+        Draw.Circle(this.position, this.size, this.color);
     }
+
     Move()
     {
-        this.position.CalVectorKeep("+", this.moveVector);
+        this.position.CalVectorKeep("+", this.velocity);
     }
-    Colleder(obj)
+}
+
+class Collider
+{
+    static typeofCollider = ["Box", "Circle"];
+
+    static ColliderOverlap(objA, objB) //미완성
     {
-        
+        var MakeRealCollider = Collider.MakeRealCollider;
+        var aRealCol = MakeRealCollider(objA.collider, objA.position);
+        var bRealCol = MakeRealCollider(objB.collider, objB.position);
+        var CheckTypeNum = Collider.CheckTypeNum;
+        if (CheckTypeNum(aRealCol) > CheckTypeNum(bRealCol))
+        {
+            var temp = aRealCol;
+            aRealCol = bRealCol;
+            bRealCol = temp;
+        }
+    }
+
+    static MakeRealCollider(collider, position)
+    {
+        var typeofCollider = Collider.typeofCollider;
+        var type = collider.type
+        switch (type)
+        {
+            case typeofCollider[0]:
+                return {type: type, start: position.CalVector("+", collider.start), end: position.CalVector("+", collider.end)};
+            case typeofCollider[1]:
+                return {type: type, size: collider.size, offset: position.CalVector("+", collider.offset)};
+            default:
+                return null;
+        }
+    }
+
+    static CheckTypeNum(collider)
+    {
+        var typeofCollider = Collider.typeofCollider;
+        var type = collider.type;
+        for (let i = 0; i < typeofCollider.length; i++)
+        {
+            if (type == typeofCollider[i])
+                return i;
+        }
+        return null;
     }
 }
 
 function CheckObjOut(obj)
 {
-    var pos = obj.position.vector;
-    return Math.abs(pos.x - halfWidth) > halfWidth + obj.size || Math.abs(pos.y - halfHeight) > halfHeight + obj.size;
+    var pos = obj.position;
+    var width_Half = Canvas.width_Half, height_Half = Canvas.height_Half;
+    return Math.abs(pos.x - width_Half) > width_Half + obj.size || Math.abs(pos.y - height_Half) > height_Half + obj.size;
 }
 
 class GameManager
 {
     constructor()
     {
-        this.player = new player(new Vector({x: halfWidth, y: halfHeight}), 15);
+        this.player = new Player(new Vector(Canvas.width_Half, Canvas.height_Half), 15);
         this.bullets = [];
         this.enemies = [];
-        canvas.onclick = function(event){
+        Canvas.self.onclick = function(event){
             var playerPos = gm.player.position;
-            var finalMove = new Vector({x: event.clientX, y: event.clientY}).CalVector("-", playerPos).Normalization();
-            gm.bullets.push(new bullet(playerPos.Copy(), 8, finalMove, 5))
+            var direction = new Vector(event.clientX, event.clientY).CalVector("-", playerPos).Normalization();
+            gm.bullets.push(new Bullet(playerPos.Copy(), 8, direction, 5))
         }
+        this.Random = Random.Random;
+        this.Precentage = function() { return this.Random(100); };
+        this.RandomBool = Random.RandomBool;
     }
 
     Update()
     {
-        if (Math.random() < 0.004)
+        if (this.Precentage() < 4)
         {
-            var x = Math.random() * width, y = Math.random() * height;
-            this.enemies.push(new Enemy(new Vector({x: x, y: y}), 5, 3, this.player));
+            var EnemyHP = Random.RandomInt(10);
+            var EnemySize = EnemyHP * 3;
+            var width = Canvas.width, height = Canvas.height;
+            var x = this.Random(width), y = this.Random(height);
+            if (this.RandomBool())
+                x = this.RandomBool() ? -EnemySize : width + EnemySize;
+            else
+                y = this.RandomBool() ? -EnemySize : height + EnemySize;
+            this.enemies.push(new Enemy(new Vector(x, y), EnemyHP, 1, this.player));
         }
-        for (let index = 0; index < this.enemies.length; index++)
+        for (let i = 0; i < this.enemies.length; i++)
         {
-            var enemy = this.enemies[index];
+            var enemy = this.enemies[i];
             enemy.Move();
             if (CheckObjOut(enemy))
             {
-                this.enemies.splice(index,1);
-                index--;
+                this.enemies.splice(i,1);
+                i--;
                 continue;
             }
+            enemy.UpdateSize();
         }
-        for (let index = 0; index < this.bullets.length; index++)
+        for (let i = 0; i < this.bullets.length; i++)
         {
-            var bullet = this.bullets[index];
+            var bullet = this.bullets[i];
             bullet.Move();
             if (CheckObjOut(bullet))
             {
-                this.bullets.splice(index, 1);
-                index--;
+                this.bullets.splice(i, 1);
+                i--;
             }
         }
         
@@ -195,11 +306,11 @@ class GameManager
 
     Draw()
     {
-        context.clearRect(0, 0, width, height);
-        for (let index = 0; index < this.bullets.length; index++)
-            this.bullets[index].Draw();
-        for (let index = 0; index < this.enemies.length; index++)
-            this.enemies[index].Draw();
+        Canvas.context.clearRect(0, 0, Canvas.width, Canvas.height);
+        for (let i = 0; i < this.bullets.length; i++)
+            this.bullets[i].Draw();
+        for (let i = 0; i < this.enemies.length; i++)
+            this.enemies[i].Draw();
         this.player.Draw();
     }
 
@@ -209,8 +320,6 @@ class GameManager
         this.Draw();
     }
 }
-
-var e = [];
 
 var gm = new GameManager();
 var loop = setInterval(() => { gm.Loop(); }, 1000 / 60);
