93bb730ecb8dc74facad105523f86cbc4a80858c
GTByun <byungt02@naver.com>
2022-05-19 11:11:06 +0900

    [GT] Vector 클래스 추가

--- Lecture10/canvasgame.js
+++ Lecture10/canvasgame.js
@@ -4,12 +4,83 @@ var width = canvas.width;
 var height = canvas.height;
 var halfWidth = canvas.width / 2;
 var halfHeight = canvas.height / 2;
+var VectorInitial = {x: null, y: null};
+var PI = Math.PI;
 
-function ObjDraw(x, y, size, color)
+class Vector
 {
-    var halfSize = size / 2;
+    constructor(vector)
+    {
+        this.vector = vector;
+    }
+
+    CalVector(what, vector)
+    {
+        switch (what)
+        {
+            case "-":
+                return new Vector({x: this.vector.x - vector.vector.x, y: this.vector.y - vector.vector.y});
+            default:
+                return new Vector(VectorInitial);
+        }
+    }
+    
+    CalVectorKeep(what, vector)
+    {
+        switch (what)
+        {
+            case "+":
+                this.vector = {x: this.vector.x + vector.vector.x, y: this.vector.y + vector.vector.y};
+                break;
+            default:
+                break;
+        }
+    }
+
+    CalScalar(what, scalar)
+    {
+        switch (what)
+        {
+            case "*":
+                return new Vector({x: this.vector.x * scalar, y: this.vector.y * scalar});
+            case "/":
+                return new Vector({x: this.vector.x / scalar, y: this.vector.y / scalar});
+            default:
+                return new Vector(VectorInitial);
+        }
+    }
+
+    Normalization()
+    {
+        return this.CalScalar("/", Math.sqrt(Math.pow(this.vector.x, 2) + Math.pow(this.vector.y, 2)))
+    }
+
+    Copy()
+    {
+        return new Vector(this.vector);
+    }
+}
+
+function Hex2(int)
+{
+    var result = int.toString(16);
+    return result.length == 1 ? "0" + result : result;
+}
+
+function RandomInt256()
+{
+    return Math.random() * 256 | 0;
+}
+
+function RGB(red, green, blue)
+{
+    return "#" + Hex2(red) + Hex2(green) + Hex2(blue);
+}
+
+function ObjDraw(position, size, color)
+{
     context.beginPath();
-    context.arc(x - halfSize, y - halfSize, size, 0, 2 * Math.PI);
+    context.arc(position.vector.x, position.vector.y, size, 0, 2 * PI);
     context.fillStyle = color;
     context.fill();
     context.closePath();
@@ -17,107 +88,129 @@ function ObjDraw(x, y, size, color)
 
 class player
 {
-    constructor(x, y, size, speed)
+    constructor(position, size)
     {
-        this.x = x;
-        this.y = y;
+        this.position = position;
         this.size = size;
-        this.speed = speed;
     }
     Draw()
     {
-        ObjDraw(this.x, this.y, this.size, "blue");
+        ObjDraw(this.position, this.size, "blue");
     }
 }
 
 class bullet
 {
-    constructor(x, y, size, xMove, yMove, speed)
+    constructor(position, size, moveVector, speed)
     {
-        this.x = x;
-        this.y = y;
+        this.position = position;
         this.size = size;
         this.halfSize = size / 2;
-        this.xMove = xMove * speed;
-        this.yMove = yMove * speed;
+        this.moveVector = moveVector.CalScalar("*", speed);
     }
     Draw()
     {
-        ObjDraw(this.x, this.y, this.size, "red");
+        ObjDraw(this.position, this.size, "red");
     }
     Move()
     {
-        this.x += this.xMove;
-        this.y += this.yMove;
+        this.position.CalVectorKeep("+", this.moveVector);
     }
 }
 
 class Enemy
 {
-    constructor(x, y, hp, speed)
+    constructor(position, hp, speed, player)
     {
-        this.x = x;
-        this.y = y;
+        this.position = position;
         this.hp = hp;
-        this.speed = speed;
+        this.moveVector = player.position.CalVector("-", this.position).Normalization().CalScalar("*", speed)
+        this.color = RGB(RandomInt256(), RandomInt256(), RandomInt256());
+    }
+    Draw()
+    {
+        this.size = this.hp * 3;
+        ObjDraw(this.position, this.size, this.color);
     }
     Move()
     {
-
+        this.position.CalVectorKeep("+", this.moveVector);
     }
+    Colleder(obj)
+    {
+        
+    }
+}
+
+function CheckObjOut(obj)
+{
+    var pos = obj.position.vector;
+    return Math.abs(pos.x - halfWidth) > halfWidth + obj.size || Math.abs(pos.y - halfHeight) > halfHeight + obj.size;
 }
 
 class GameManager
 {
     constructor()
     {
+        this.player = new player(new Vector({x: halfWidth, y: halfHeight}), 15);
+        this.bullets = [];
+        this.enemies = [];
         canvas.onclick = function(event){
-            const x = event.clientX;
-            const y = event.clientY;
-            var xMove = x - p.x, yMove = y - p.y;
-            var distance = Math.sqrt(Math.pow(xMove, 2) + Math.pow(yMove, 2));
-            xMove /= distance;
-            yMove /= distance;
-            b.push(new bullet(p.x, p.y, 8, xMove, yMove, 5));
+            var playerPos = gm.player.position;
+            var finalMove = new Vector({x: event.clientX, y: event.clientY}).CalVector("-", playerPos).Normalization();
+            gm.bullets.push(new bullet(playerPos.Copy(), 8, finalMove, 5))
         }
     }
 
     Update()
     {
-        for (let index = 0; index < b.length; index++)
+        if (Math.random() < 0.004)
         {
-            b[index].Move();
-            if (Math.abs(b[index].x - halfWidth) > halfWidth + b[index].halfSize || Math.abs(b[index].y - halfHeight) > halfHeight + b[index].halfSize)
+            var x = Math.random() * width, y = Math.random() * height;
+            this.enemies.push(new Enemy(new Vector({x: x, y: y}), 5, 3, this.player));
+        }
+        for (let index = 0; index < this.enemies.length; index++)
+        {
+            var enemy = this.enemies[index];
+            enemy.Move();
+            if (CheckObjOut(enemy))
             {
-                b.splice(index, 1);
+                this.enemies.splice(index,1);
+                index--;
+                continue;
+            }
+        }
+        for (let index = 0; index < this.bullets.length; index++)
+        {
+            var bullet = this.bullets[index];
+            bullet.Move();
+            if (CheckObjOut(bullet))
+            {
+                this.bullets.splice(index, 1);
                 index--;
             }
         }
-            
+        
     }
 
     Draw()
     {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        for (let index = 0; index < b.length; index++)
-            b[index].Draw();
-        p.Draw();
+        context.clearRect(0, 0, width, height);
+        for (let index = 0; index < this.bullets.length; index++)
+            this.bullets[index].Draw();
+        for (let index = 0; index < this.enemies.length; index++)
+            this.enemies[index].Draw();
+        this.player.Draw();
     }
 
     Loop()
     {
-        gm.Update();
-        gm.Draw();
+        this.Update();
+        this.Draw();
     }
 }
-var p = new player(320, 240, 15, 3);
-var b = [];
+
 var e = [];
 
 var gm = new GameManager();
-function Loop()
-{
-    gm.Update();
-    gm.Draw();
-}
-var loop = setInterval(gm.Loop, 1000 / 60);
+var loop = setInterval(() => { gm.Loop(); }, 1000 / 60);
